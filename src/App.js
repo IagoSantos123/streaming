1b6d94b24ee403cf8417272b1c13ebed95c508ac
Iasmin Santos <iasmin.santos.ext@synchro.com.br>
2023-01-20 11:45:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import tmdb from './tmdb';
+import MovieRow from './componentes/MovieRow';
+import featuredMovie from './componentes/featuredMovie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default () =>{
+
+    const [movielist, setMovieList] = useState([])
+    const [featuredData, setfeaturedData] = useState(null);
+    useEffect(()=>{
+        const loadAll = async () => {
+            //PEGANDO A LISTA TOTAL
+            let list = await tmdb.getHomelist();
+            setMovieList(list);
+
+            //PEGANDO Featured
+            let originals = list.filter(i=>i.slug === 'originals');
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv');
+            setfeaturedData(chosenInfo);
+
+            
+        }
+        loadAll();
+    }, []);
+
+    return (
+        <div className="page">
+
+            {featuredData &&
+                <featuredMovie item={featuredData}/>
+                
+            }
+            <section className='lists'>
+                {movielist.map((item, key)=>(
+                    <MovieRow key={key} title={item.title} items={item.items}/>
+                ))}
+            </section>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/componentes/featuredMovie.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './featureMovie.css';
+
+export default () => {
+    return(
+        <div>
+            FILME EM DESTAQUE
+        </div>
+    )
+}
